4df4d3d9b7af3fdf8002ed31879f90c7dfae0d7a
Felix Krull <f_krull@gmx.de>
2012-09-21 03:45:17 +0200

    Import better multiarch patch.

--- .pc/deadsnakes-assert-use-system-ffi.diff/setup.py
+++ .pc/deadsnakes-assert-use-system-ffi.diff/setup.py
@@ -354,12 +354,33 @@ class PyBuildExt(build_ext):
                 return platform
         return sys.platform
 
+    def add_multiarch_paths(self):
+        # Debian/Ubuntu multiarch support.
+        # https://wiki.ubuntu.com/MultiarchSpec
+        if not find_executable('dpkg-architecture'):
+            return
+        tmpfile = os.path.join(self.build_temp, 'multiarch')
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        ret = os.system(
+            'dpkg-architecture -qDEB_HOST_MULTIARCH > %s 2> /dev/null' %
+            tmpfile)
+        try:
+            if ret >> 8 == 0:
+                with open(tmpfile) as fp:
+                    multiarch_path_component = fp.readline().strip()
+                add_dir_to_list(self.compiler.library_dirs,
+                                '/usr/lib/' + multiarch_path_component)
+                add_dir_to_list(self.compiler.include_dirs,
+                                '/usr/include/' + multiarch_path_component)
+        finally:
+            os.unlink(tmpfile)
+
     def detect_modules(self):
         # On Debian /usr/local is always used, so we don't include it twice
         #add_dir_to_list(self.compiler.library_dirs, '/usr/local/lib')
         #add_dir_to_list(self.compiler.include_dirs, '/usr/local/include')
-        add_dir_to_list(self.compiler.include_dirs,
-                        '/usr/include/%s' % os.environ['DEB_HOST_MULTIARCH'])
+        self.add_multiarch_paths()
 
         # Add paths specified in the environment variables LDFLAGS and
         # CPPFLAGS for header and library files.
@@ -412,8 +433,6 @@ class PyBuildExt(build_ext):
         lib_dirs = self.compiler.library_dirs + [
             '/lib64', '/usr/lib64',
             '/lib', '/usr/lib',
-            '/lib/%s' % os.environ['DEB_HOST_MULTIARCH'],
-            '/usr/lib/%s' % os.environ['DEB_HOST_MULTIARCH'],
             ]
         inc_dirs = self.compiler.include_dirs + ['/usr/include']
         exts = []
